7d584a4e762bb7672870b67c5116a2f5e3da74b9
Michael Schuster <michael.schuster@qiminfo.ch>
2023-12-01 17:51:04 +0100

    reformat

--- lib/day1.ml
+++ lib/day1.ml
@@ -1,15 +1,20 @@
 open Base
+
 let read_lines filename =
   let ic = Stdio.In_channel.create filename in
   let try_read () =
-    try Some (Stdio.In_channel.input_line_exn ic) with End_of_file -> None
+    try Some (Stdio.In_channel.input_line_exn ic) with
+    | End_of_file -> None
   in
   let rec loop acc =
     match try_read () with
     | Some line -> loop (line :: acc)
-    | None -> Stdio.In_channel.close ic; List.rev acc
+    | None ->
+      Stdio.In_channel.close ic;
+      List.rev acc
   in
   loop []
+;;
 
 let filename = "../input/day1.txt"
 let lines = read_lines filename
@@ -26,31 +31,38 @@ let string_to_number = function
   | s when String.is_prefix ~prefix:"eight" s -> "8"
   | s when String.is_prefix ~prefix:"nine" s -> "9"
   | _ -> ""
+;;
 
 let rec line_to_number = function
   | "" -> ""
-  | s -> 
-      let next = String.sub s ~pos:1 ~len:(String.length s -  1)   in
-      string_to_number s ^ line_to_number next
+  | s ->
+    let next = String.sub s ~pos:1 ~len:(String.length s - 1) in
+    string_to_number s ^ line_to_number next
+;;
 
 let sanatized_lines = List.map lines ~f:line_to_number
 
 let line_digit line =
-  let list_char = String.to_list line in 
+  let list_char = String.to_list line in
   let filtered = List.filter list_char ~f:Char.is_digit in
   String.of_char_list filtered
+;;
 
 let input_digit input = List.map input ~f:line_digit
 
 let rec get_line_value = function
   | [] -> []
-  | current :: rest -> 
-      ((String.get current 0 |> Char.escaped) ^ (String.get current (String.length current - 1) |> Char.escaped))
-      :: (get_line_value rest)
+  | current :: rest ->
+    ((String.get current 0 |> Char.escaped)
+     ^ (String.get current (String.length current - 1) |> Char.escaped))
+    :: get_line_value rest
+;;
 
-let rec sum_values (values: string list) = match values with
+let rec sum_values (values : string list) =
+  match values with
   | [] -> 0
   | current :: rest -> Int.of_string current + sum_values rest
+;;
 
 let result1 = input_digit lines |> get_line_value |> sum_values |> Int.to_string
-let result2 = input_digit sanatized_lines |> get_line_value |> sum_values |> Int.to_string 
+let result2 = input_digit sanatized_lines |> get_line_value |> sum_values |> Int.to_string
